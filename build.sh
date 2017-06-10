79eb0069ad3fa3f9c4055273f478fc593ef2c4ae
Bubba <rob@stechstudio.com>
2017-06-10 12:56:42 -0400

    PHP Build Script

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+if [ $# -lt 1 ]; then
+  echo
+  echo "Usage: $0 VERSION"
+  echo "Build shared libraries for php and its dependencies via containers"
+  echo
+  echo "Please specify the php VERSION, e.g. 7.1.5, 7.0.20, 5.6.30"
+  echo
+  exit 1
+fi
+VERSION_PHP="$1"
+
 # Is docker available?
 if ! type docker >/dev/null; then
   echo "Please install docker"
@@ -9,4 +20,4 @@ fi
 
 echo "Building php..."
 docker build -t php-dev-lambda amazonlinux
-docker run -it -v $PWD:/packaging amazonlinux /bin/bash
+docker run --rm -e "VERSION_PHP=${VERSION_PHP}" -v $PWD:/packaging php-dev-lambda sh -c "/packaging/build/php.sh"
