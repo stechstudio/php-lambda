be9e77375448ab3914a29beebb82b85bde6ee683
Bubba <rob@stechstudio.com>
2018-12-21 08:02:15 -0500

    zlib make file

--- docker/build/zlib.mk
+++ docker/build/zlib.mk
@@ -1,9 +1,23 @@
+.DEFAULT_GOAL := make_zlib
 url_zlib = http://zlib.net/zlib-${VERSION_ZLIB}.tar.xz
 build_dir_zlib = ${DEPS}/zlib
+ifeq ($(suffix $(url_zlib)), .gz)
+	zlib_args=xzC
+else
+	ifeq ($(suffix $(url_zlib)), tgz)
+		zlib_args=xzC
+	else
+		ifeq ($(suffix $(url_zlib)), bz2)
+			zlib_args=xjC
+		else
+			zlib_args=xJC
+		endif
+	endif
+endif
 
 fetch_zlib:
 	mkdir -p ${build_dir_zlib}
-	curl -Ls ${url_zlib} | tar xJC ${build_dir_zlib} --strip-components=1
+	curl -Ls ${url_zlib} | tar $(zlib_args) ${build_dir_zlib} --strip-components=1
 
 configure_zlib:
 	cd ${build_dir_zlib} && \
